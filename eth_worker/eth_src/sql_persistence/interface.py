6eb99d16dd54472c20e012dd18adb12be8bb3898
Cral_Cactus <marian.pb@abv.bg>
2024-07-22 17:00:02 +0300

    Finished sql persistence

--- eth_worker/eth_src/sql_persistence/interface.py
+++ eth_worker/eth_src/sql_persistence/interface.py
@@ -282,47 +282,282 @@ class SQLPersistenceInterface(object):
             except ValueError:
                 pass
 
-    def remove_all_posterior_dependencies(self, prior_task_uuid: UUID) -> UUIDList:
-        prior_task = self.get_task_from_uuid(task_uuid=prior_task_uuid)
+    def get_duplicates(self, min_task_id, max_task_id):
 
-        posterior_task_uuids = [t.uuid for t in prior_task.posterior_tasks]
+        from sqlalchemy.sql import text
 
-        prior_task.posterior_tasks = []
+        query = text(
+            """
+                SELECT blockchain_task.id as task_id,
+                  (SELECT COUNT(*)
+                    FROM blockchain_transaction
+                    WHERE blockchain_task_id = blockchain_task.id AND _status = 'SUCCESS'
+                    ) as txn_count
+                FROM blockchain_task
+                RIGHT JOIN blockchain_transaction
+                ON  blockchain_transaction.blockchain_task_id = blockchain_task.id
+                WHERE blockchain_task.id > :min_task_id and blockchain_task.id < :max_task_id
+                GROUP BY blockchain_task.id
+                HAVING (
+                  SELECT COUNT(*)
+                  FROM blockchain_transaction
+                  WHERE blockchain_task_id = blockchain_task.id AND _status = 'SUCCESS'
+                  ) > 1;
+            """
+        )
+        res = self.session.execute(query, {'min_task_id': min_task_id, 'max_task_id': max_task_id})
+
+        duplicated_tasks = [row for row in res]
+        return duplicated_tasks
+
+    def get_task_from_uuid(self, task_uuid):
+        return self.session.query(BlockchainTask).filter_by(uuid=task_uuid).first()
+
+    def get_task_from_id(self, task_id):
+        return self.session.query(BlockchainTask).get(task_id)
+
+    def _filter_minmax_task_ids_maybe(self, query, min_task_id, max_task_id):
+        if min_task_id:
+            query = query.filter(BlockchainTask.id > min_task_id)
+        if max_task_id:
+            query = query.filter(BlockchainTask.id < max_task_id)
+
+        return query
+
+    def _get_tasks_by_status(self, status, min_task_id, max_task_id):
+        query = self.session.query(BlockchainTask) \
+            .filter(BlockchainTask.status == status) \
+            .order_by(BlockchainTask.id.asc())
+        query = self._filter_minmax_task_ids_maybe(query, min_task_id, max_task_id)
+
+        return query.all()
+
+    def get_failed_tasks(self, min_task_id=None, max_task_id=None):
+        return self._get_tasks_by_status('FAILED', min_task_id, max_task_id)
+
+    def get_pending_tasks(self, min_task_id=None, max_task_id=None):
+        return self._get_tasks_by_status('PENDING', min_task_id, max_task_id)
+
+    def get_unstarted_tasks(self, min_task_id=None, max_task_id=None):
+        return self._get_tasks_by_status('UNSTARTED', min_task_id, max_task_id)
+
+    def get_unstarted_posteriors(self, task_uuid):
+
+        task = self.get_task_from_uuid(task_uuid=task_uuid)
+
+        if not task:
+            return None
+
+        if task:
+            unstarted_posteriors = []
+            for posterior in task.posterior_tasks:
+                if posterior.status == 'UNSTARTED':
+                    unstarted_posteriors.append(posterior)
+
+            return unstarted_posteriors
+
+    def get_unsatisfied_prior_tasks(self, task_uuid):
+
+        task = self.get_task_from_uuid(task_uuid=task_uuid)
+
+        if not task:
+            return None
+
+        unsatisfied = []
+        for prior in task.prior_tasks:
+            if prior.status != 'SUCCESS':
+                unsatisfied.append(prior)
+
+        return unsatisfied
+
+    def get_signing_wallet_object(self, signing_address, encrypted_private_key):
+        if signing_address:
+
+            signing_wallet_obj = self.get_wallet_by_address(signing_address)
+
+            if signing_wallet_obj is None:
+                raise Exception('Address {} not found'.format(signing_address))
+
+        elif encrypted_private_key:
+
+            signing_wallet_obj = self.get_wallet_by_encrypted_private_key(encrypted_private_key)
+
+            if not signing_wallet_obj:
+                signing_wallet_obj = self.create_blockchain_wallet_from_encrypted_private_key(
+                    encrypted_private_key=encrypted_private_key
+                )
+        else:
+            raise Exception("Must provide encrypted private key")
+
+        return signing_wallet_obj
+
+
+    def create_blockchain_wallet_from_encrypted_private_key(self, encrypted_private_key):
+
+        private_key = BlockchainWallet.decrypt_private_key(encrypted_private_key)
+        return self.create_blockchain_wallet_from_private_key(private_key)
+
+    def create_blockchain_wallet_from_private_key(self, private_key,
+                                                  allow_existing=False,
+                                                  wei_target_balance=0,
+                                                  wei_topup_threshold=0,
+                                                  ):
+
+        address = BlockchainWallet.address_from_private_key(private_key)
+
+        existing_wallet = self.session.query(BlockchainWallet).filter_by(address=address).first()
+        if existing_wallet:
+            if allow_existing:
+                return existing_wallet
+            else:
+                raise WalletExistsError("Account for provided private key already exists")
+
+        wallet = BlockchainWallet(
+            private_key=private_key,
+            wei_target_balance=wei_target_balance,
+            wei_topup_threshold=wei_topup_threshold)
+
+        self.session.add(wallet)
 
         self.session.commit()
 
-        return posterior_task_uuids
+        return wallet
 
-    def increment_task_invocations(self, task_uuid: UUID):
+    def create_external_transaction(self, status, block, hash, contract_address, is_synchronized_with_app, recipient_address, sender_address, amount):
+        transaction_object = BlockchainTransaction(
+            _status = status,
+            block = block,
+            hash = hash,
+            contract_address = contract_address,
+            is_synchronized_with_app = is_synchronized_with_app,
+            recipient_address = recipient_address,
+            sender_address = sender_address,
+            amount = amount,
+            is_third_party_transaction = True # External transaction will always be third party
+        )   
+        self.session.add(transaction_object)
+        self.session.commit()
+        return transaction_object
 
-        task = self.get_task_from_uuid(task_uuid=task_uuid)
-        if task:
-            task.previous_invocations = (task.previous_invocations or 0) + 1
-            self.session.commit()
+    def mark_transaction_as_completed(self, transaction):
+        transaction.is_synchronized_with_app = True
+        self.session.commit()
+        return transaction
 
-    def get_serialised_task_from_uuid(self, uuid):
-        task = self.get_task_from_uuid(uuid)
+    def get_transaction_by_hash(self, hash):
+        return self.session.query(BlockchainTransaction).filter(BlockchainTransaction.hash == hash).first()
 
-        if task is None:
-            return None
+    def create_new_blockchain_wallet(self, wei_target_balance=0, wei_topup_threshold=0, private_key=None):
 
-        base_data = {
-            'id': task.id,
-            'status': task.status,
-            'prior_tasks': [task.uuid for task in task.prior_tasks],
-            'posterior_tasks': [task.uuid for task in task.posterior_tasks],
-            'transactions': [transaction.id for transaction in task.transactions]
+        if private_key:
+            return self.create_blockchain_wallet_from_private_key(
+                private_key,
+                True,
+                wei_target_balance,
+                wei_topup_threshold,
+            )
+
+        wallet = BlockchainWallet(wei_target_balance=wei_target_balance,
+                                  wei_topup_threshold=wei_topup_threshold)
+
+        self.session.add(wallet)
+
+        self.session.commit()
+
+        return wallet
+
+
+    def get_all_wallets(self):
+        return self.session.query(BlockchainWallet).all()
+
+    def get_wallet_by_address(self, address):
+        return self.session.query(BlockchainWallet).filter(BlockchainWallet.address == address).first()
+
+    def get_wallet_by_encrypted_private_key(self, encrypted_private_key):
+         return self.session.query(BlockchainWallet).filter(
+             BlockchainWallet.encrypted_private_key == encrypted_private_key).first()
+
+    def set_wallet_last_topup_task_uuid(self, wallet_address, task_uuid):
+        wallet = self.get_wallet_by_address(wallet_address)
+        wallet.last_topup_task_uuid = task_uuid
+
+        self.session.commit()
+
+    def check_if_synchronization_filter_exists(self, contract_address, filter_parameters):
+        filter = self.session.query(SynchronizationFilter).filter(
+            SynchronizationFilter.contract_address == contract_address, SynchronizationFilter.filter_parameters == filter_parameters).first()
+        if filter == None:
+            return False
+        return filter
+
+    def get_transaction_sync_metrics(self):
+        def __query_tuple_list_to_dict__(result):
+            return { key: value for (key, value) in result }
+
+        unsynchronized_transaction_count = self.session.query(
+            BlockchainTransaction.contract_address, func.count(BlockchainTransaction.id))\
+            .group_by(BlockchainTransaction.contract_address)\
+            .filter(BlockchainTransaction.is_synchronized_with_app == False)\
+            .all()
+
+        synchronized_transaction_count = self.session.query(
+            BlockchainTransaction.contract_address, func.count(BlockchainTransaction.id))\
+            .group_by(BlockchainTransaction.contract_address)\
+            .filter(BlockchainTransaction.is_synchronized_with_app == True)\
+            .all()
+        
+        last_time_synchronized = self.session.query(SynchronizationFilter.contract_address, SynchronizationFilter.updated).all()
+
+        return {
+            'unsynchronized_transaction_count': __query_tuple_list_to_dict__(unsynchronized_transaction_count),
+            'synchronized_transaction_count': __query_tuple_list_to_dict__(synchronized_transaction_count),
+            'last_time_synchronized': __query_tuple_list_to_dict__(last_time_synchronized)
         }
+        
+    # Utility function for get_failed_callbacks
+    def __aggregate_tuple_list__(self, response):
+        result = {}
+        for address, hash in response:
+            if hash:
+                if address in result:
+                    result[address].append(hash)
+                else:
+                    result[address] = [hash]
+        return result
 
-        if task.successful_transaction:
+    def get_failed_callbacks(self):
+        failed_callbacks = self.session.query(
+            BlockchainTransaction.contract_address, BlockchainTransaction.hash)\
+            .filter(BlockchainTransaction.is_synchronized_with_app == False)\
+            .all()
+        return self.__aggregate_tuple_list__(failed_callbacks)
 
-            transaction_data = {
-                'successful_hash': task.successful_transaction.hash,
-                'successful_block': task.successful_transaction.block,
-                'contract_address': task.successful_transaction.contract_address
-            }
+    def add_transaction_filter(self, contract_address, contract_type, filter_parameters, filter_type, decimals, block_epoch):
+        filter = SynchronizationFilter(contract_address=contract_address, contract_type=contract_type, filter_parameters=filter_parameters, max_block=block_epoch, filter_type=filter_type, decimals=decimals)
+        self.session.add(filter)
+        self.session.commit()
+        return filter
 
-            return {**transaction_data, **base_data}
+    def set_filter_max_block(self, filter_id, block):
+        filter = self.session.query(SynchronizationFilter).filter(SynchronizationFilter.id == filter_id).first()
+        filter.max_block = block
+        self.session.commit()
+        return True
 
-        else:
-            return base_data
+    def get_synchronization_filter_by_id(self, filter_id):
+        return self.session.query(SynchronizationFilter).filter(SynchronizationFilter.id == filter_id).first()
+
+    def get_synchronization_filter_by_address(self, address):
+        return self.session.query(SynchronizationFilter).filter(SynchronizationFilter.contract_address == address).first()
+
+    def get_all_synchronization_filters(self):
+        return self.session.query(SynchronizationFilter).all()
+
+    def __init__(self, red, session, first_block_hash, PENDING_TRANSACTION_EXPIRY_SECONDS=config.CHAINS[chain]['PENDING_TRANSACTION_EXPIRY_SECONDS']):
+        self.red = red
+
+        self.session = session
+        
+        self.first_block_hash = first_block_hash
+
+        self.PENDING_TRANSACTION_EXPIRY_SECONDS = PENDING_TRANSACTION_EXPIRY_SECONDS
