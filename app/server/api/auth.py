282ab6ac744df2de0d325de261cab0a2c975b5d4
Cral_Cactus <marian.pb@abv.bg>
2024-07-23 21:23:42 +0300

    Ready with half auth server api

--- app/server/api/auth.py
+++ app/server/api/auth.py
@@ -194,73 +194,302 @@ class RegisterAPI(MethodView):
         return make_response(jsonify(attach_host(response_object))), 201
 
 
-class ActivateUserAPI(MethodView):
-    """
-    User Registration Resource
-    """
+        class LoginAPI(MethodView):
+        """
+        User Login Resource
+        """
+
+     def get(self):
+
+        print("process started")
+
+        challenges = [
+            ('Why don’t they play poker in the jungle?', 'Too many cheetahs.'),
+            ('What did the Buddhist say to the hot dog vendor?', 'Make me one with everything.'),
+            ('What does a zombie vegetarian eat?', 'Graaaaaaaains!'),
+            ('My new thesaurus is terrible.', 'Not only that, but it’s also terrible.'),
+            ('Why didn’t the astronaut come home to his wife?', 'He needed his space.'),
+            ('I got fired from my job at the bank today.',
+             'An old lady came in and asked me to check her balance, so I pushed her over.'),
+            ('I like to spend every day as if it’s my last',
+             'Staying in bed and calling for a nurse to bring me more pudding.')
+        ]
+
+        challenge = random.choice(challenges)
+
+        # time.sleep(int(request.args.get('delay', 0)))
+        # from functools import reduce
+        # reduce(lambda x, y: x + y, range(0, int(request.args.get('count', 1))))
+
+        # memory_to_consume = int(request.args.get('MB', 0)) * 1000000
+        # bytearray(memory_to_consume)
+
+        ip_address = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+        user_agent = request.environ["HTTP_USER_AGENT"]
+        ip = request.environ["REMOTE_ADDR"]
+        # proxies = request.headers.getlist("X-Forwarded-For")
+        # http://esd.io/blog/flask-apps-heroku-real-ip-spoofing.html
+
+        response_object = {
+            'status': 'success',
+            'who_allows_a_get_request_to_their_auth_endpoint': 'We do.',
+            challenge[0]: challenge[1],
+            # 'metadata': {'user_agent': user_agent, 'ip': ip_address, 'otherip': ip, 'proxies': proxies},
+        }
+        return make_response(jsonify(attach_host(response_object))), 200
 
     def post(self):
-        # get the post data
+        # There is an unique case where users are using their mobile number from the App to either login or register
+        # The app uses g.active_organisation to reference user.transfer_account to send an SMS to the User.
+        # This means that g.active_organisation should default to the master_organisation
+        # For admin users, it doesn't matter as this endpoint is unauthed.
+        g.active_organisation = Organisation.master_organisation()
+
         post_data = request.get_json()
+        user = None
+        phone = None
+        email = post_data.get('username', '') or post_data.get('email', '')
+        email = email.lower() if email else ''
+        password = post_data.get('password')
+        # Default pin to password as fallback for old android versions
+        pin = post_data.get('pin', password)
+        tfa_token = post_data.get('tfa_token')
 
-        activation_token = post_data.get('activation_token')
+        password_empty = password == '' or password is None
+        pin_empty = pin == '' or pin is None
 
-        if activation_token and activation_token != 'null':
-            auth_token = activation_token.split(" ")[0]
-        else:
-            auth_token = ''
-        if auth_token:
-
-            validity_check = User.decode_single_use_JWS(activation_token, 'A')
-
-            if not validity_check['success']:
+        ratelimit_key = email or post_data.get('phone')
+        if ratelimit_key:
+            limit = rate_limit("login_"+ratelimit_key, 25)
+            if limit:
                 response_object = {
                     'status': 'fail',
-                    'message': validity_check['message']
+                    'message': f'Please try again in {limit} minutes'
+                }
+                return make_response(jsonify(response_object)), 403
+
+        # First try to match email
+        if email:
+            user = User.query.filter(func.lower(User.email)==email).execution_options(show_all=True).first()
+
+        # Now try to match the public serial number (comes in under the phone)
+        if not user:
+            public_serial_number_or_phone = post_data.get('phone')
+
+            user = User.query.filter_by(public_serial_number=public_serial_number_or_phone).execution_options(
+                show_all=True).first()
+
+        # Now try to match the phone
+        if not user:
+            try:
+                phone = proccess_phone_number(post_data.get('phone'), region=post_data.get('region'))
+            except NumberParseException as e:
+                response_object = {'message': 'Invalid Phone Number: ' + str(e)}
+                return make_response(jsonify(response_object)), 401
+
+            if phone:
+                user = User.query.filter_by(phone=phone).execution_options(show_all=True).first()
+
+        # mobile user doesn't exist so default to creating a new wallet!
+        if user is None and phone and current_app.config['ALLOW_SELF_SIGN_UP']:
+            # this is a registration from a mobile device THUS a vendor or recipient.
+            response_object, response_code = UserUtils.proccess_create_or_modify_user_request(
+                dict(phone=phone, deviceInfo=post_data.get('deviceInfo')),
+                is_self_sign_up=True,
+            )
+
+            if response_code == 200:
+                db.session.commit()
+
+            return make_response(jsonify(response_object)), response_code
+        no_password_or_pin_hash = user and not user.password_hash and not user.pin_hash
+        if post_data.get('phone') and user and user.one_time_code and (not user.is_activated or not user.pin_hash):
+            # vendor sign up with one time code or OTP verified
+            if user.one_time_code == pin:
+                response_object = {
+                    'status': 'success',
+                    'pin_must_be_set': True,
+                    'message': 'Please set your pin.'
+                }
+                return make_response(jsonify(attach_host(response_object))), 200
+
+            if not user.is_phone_verified or no_password_or_pin_hash:
+                if user.is_self_sign_up:
+                    # self sign up, resend phone verification code
+                    user.set_pin(None, False)  # resets PIN
+                    UserUtils.send_one_time_code(phone=phone, user=user)
+                db.session.commit()
+
+                if not password_empty:
+                    # The user provided a password, so probably not going through incremental login
+                    # This is a hacky way to get past the incremental-login multi-org split
+                    response_object = {
+                        'status': 'fail',
+                        'otp_verify': True,
+                        'message': 'Please verify phone number.',
+                        'error_message': 'Incorrect One Time Code.'
+                    }
+                    return make_response(jsonify(attach_host(response_object))), 200
+
+                response_object = {'message':  'Please verify phone number.', 'otp_verify': True}
+                return make_response(jsonify(attach_host(response_object))), 200
+
+        if user and user.is_activated and post_data.get('phone') and (password_empty and pin_empty):
+            # user already exists, is activated. no password or pin provided, thus request PIN screen.
+            # todo: this should check if device exists, if no, resend OTP to verify login is real.
+            response_object = {
+                'status': 'success',
+                'login_with_pin': True,
+                'message': 'Login with PIN'
+            }
+            return make_response(jsonify(attach_host(response_object))), 200
+
+        if not (email or post_data.get('phone')):
+            response_object = {
+                'status': 'fail',
+                'message': 'No username supplied'
+            }
+            return make_response(jsonify(response_object)), 401
+    
+
+        try:
+            if not (user and (pin and user.verify_pin(pin) or password and user.verify_password(password))):
+                response_object = {
+                    'status': 'fail',
+                    'message': 'Invalid username or password'
                 }
 
                 return make_response(jsonify(response_object)), 401
 
-            user = validity_check['user']
-
-            if user.is_activated:
+            if not user.is_activated:
                 response_object = {
                     'status': 'fail',
-                    'message': 'Already activated.'
+                    'is_activated': False,
+                    'message': 'Account has not been activated. Please check your emails.'
                 }
-
                 return make_response(jsonify(response_object)), 401
-
-            user.is_activated = True
+            if post_data.get('deviceInfo'):
+                deviceInfo = post_data.get('deviceInfo')
+                UserUtils.save_device_info(deviceInfo, user)
 
             auth_token = user.encode_auth_token()
 
-            db.session.flush()
+            if not auth_token:
+                response_object = {
+                    'status': 'fail',
+                    'message': 'Invalid username or password'
+                }
+                return make_response(jsonify(response_object)), 401
 
             # Possible Outcomes:
             # TFA required, but not set up
+            # TFA enabled, and user does not have valid TFA token
+            # TFA enabled, and user has valid TFA token
             # TFA not required
 
-            tfa_response_oject = tfa_logic(user, tfa_token=None)
+            tfa_response_oject = tfa_logic(user, tfa_token)
             if tfa_response_oject:
                 tfa_response_oject['auth_token'] = auth_token.decode()
-
-                db.session.commit()  # need to commit here so that is_activated = True
 
                 return make_response(jsonify(tfa_response_oject)), 401
 
             # Update the last_seen TS for this user
             user.update_last_seen_ts()
 
-            response_object = create_user_response_object(user, auth_token, 'Successfully activated.')
+            response_object = create_user_response_object(user, auth_token, 'Successfully logged in.')
 
             db.session.commit()
 
-            return make_response(jsonify(attach_host(response_object))), 201
+            return make_response(jsonify(attach_host(response_object))), 200
+
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            raise e
+
+
+class LogoutAPI(MethodView):
+    """
+    Logout Resource
+    """
+
+    def post(self):
+        # get auth token
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            auth_token = auth_header.split(" ")[0]
+        else:
+            auth_token = ''
+        auth_token = auth_header.split("|")[0]
+        if auth_token:
+            resp = User.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                # mark the token as blacklisted
+                blacklist_token = BlacklistToken(token=auth_token, user_id=resp['id'])
+                try:
+                    # insert the token
+                    db.session.add(blacklist_token)
+                    db.session.commit()
+                    response_object = {
+                        'status': 'success',
+                        'message': 'Successfully logged out.'
+                    }
+                    return make_response(jsonify(attach_host(response_object))), 200
+                except Exception as e:
+                    response_object = {
+                        'status': 'fail',
+                        'message': e
+                    }
+                    return make_response(jsonify(attach_host(response_object))), 200
+            else:
+                response_object = {
+                    'status': 'fail',
+                    'message': resp
+                }
+                return make_response(jsonify(response_object)), 401
 
         else:
             response_object = {
                 'status': 'fail',
                 'message': 'Provide a valid auth token.'
             }
+            return make_response(jsonify(response_object)), 403
+
+
+class RequestPasswordResetEmailAPI(MethodView):
+    """
+    Password Reset Email Resource
+    """
+
+    def post(self):
+        # get the post data
+        post_data = request.get_json()
+
+        email = post_data.get('email', '')
+        email = email.lower() if email else ''
+        if not email:
+            response_object = {
+                'status': 'fail',
+                'message': 'No email supplied'
+            }
+
             return make_response(jsonify(response_object)), 401
+
+        limit = rate_limit("password_reset_"+email, 25)
+        if limit:
+            response_object = {
+                'status': 'fail',
+                'message': f'Please try again in {limit} minutes'
+            }
+            return make_response(jsonify(response_object)), 403
+
+        user = User.query.filter(func.lower(User.email)==email).execution_options(show_all=True).first()
+
+        if user:
+            user.reset_password()
+
+        response_object = {
+            'status': 'success',
+            'message': 'Reset email sent'
+        }
+
+        return make_response(jsonify(attach_host(response_object))), 200
