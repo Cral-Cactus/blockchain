f212cc6157e2e3a18e96abda8af066674c2c52c3
Cral_Cactus <marian.pb@abv.bg>
2024-07-25 18:17:03 +0300

    Created more server utils again

---
+++ app/server/utils/phone.py
@@ -0,0 +1,110 @@
+import phonenumbers
+import enum
+import sentry_sdk
+
+from flask import current_app, g
+from server import twilio_client, messagebird_client, africastalking_client
+from server.utils.executor import standard_executor_job
+from server.utils.internationalization import i18n_for
+
+def proccess_phone_number(phone_number, region=None, ignore_region=False):
+    """
+    Parse any given phone number.
+    :param phone_number: int
+    :param region: ISO 3166-1 alpha-2 codes
+    :param ignore_region: Boolean. True returns original phone
+    :return:
+    """
+    from server.models.organisation import Organisation
+
+    if phone_number is None:
+        return None
+
+    if ignore_region:
+        return phone_number
+
+    if region is None:
+        try:
+            region = g.active_organisation.country_code
+        except AttributeError:
+            region = Organisation.master_organisation().country_code
+
+    if not isinstance(phone_number, str):
+        try:
+            phone_number = str(int(phone_number))
+
+        except ValueError:
+            pass
+
+    phone_number_object = phonenumbers.parse(phone_number, region)
+
+    parsed_phone_number = phonenumbers.format_number(phone_number_object, phonenumbers.PhoneNumberFormat.E164)
+
+    return parsed_phone_number
+
+
+class ChannelType(enum.Enum):
+    TWILIO = "tw"
+    AFRICAS_TALKING = "at"
+    MESSAGEBIRD = "mb"
+
+# just checking by area code may break down one day since multiple countries share the same country codes...
+def channel_for_number(phone):
+    if phone.startswith("+1"):
+        return ChannelType.TWILIO
+    if phone.startswith("+254"):
+        return ChannelType.AFRICAS_TALKING
+    else:
+        # what should fallback be?
+        return ChannelType.TWILIO
+
+@standard_executor_job
+def _send_twilio_message(to_phone, message):
+    if to_phone:
+        twilio_client.api.account.messages.create(
+            to=to_phone,
+            from_=current_app.config['TWILIO_PHONE'],
+            body=message)
+
+@standard_executor_job
+def _send_messagebird_message(to_phone, message):
+    if to_phone:
+        messagebird_client.message_create(current_app.config['MESSAGEBIRD_PHONE'], to_phone, message)
+
+@standard_executor_job
+def _send_at_message(to_phone, message):
+    if to_phone:
+        # First try with custom sender Id
+        resp = africastalking_client.send(
+            message,
+            [to_phone],
+            sender_id=current_app.config.get('AT_SENDER_ID', None)
+        )
+
+        # If that fails, fallback to no sender ID
+        if resp['SMSMessageData']['Message'] == 'InvalidSenderId':
+            sentry_sdk.capture_message("InvalidSenderId {}".format(current_app.config.get('AT_SENDER_ID', None)))
+
+            resp = africastalking_client.send(
+                message,
+                [to_phone])
+
+def send_translated_message(user=None, message_key=None, phone=None, **kwargs):
+    phone_number = phone or user.phone
+    message = i18n_for(user, message_key, **kwargs)
+    send_message(phone_number, message)
+    return message
+
+def send_message(to_phone, message):
+    if to_phone:
+        if current_app.config['IS_TEST'] or current_app.config['IS_PRODUCTION']:
+            channel = channel_for_number(to_phone)
+            print(f'Sending SMS via {channel}')
+            if channel == ChannelType.TWILIO:
+                _send_twilio_message.submit(to_phone, message)
+            if channel == ChannelType.MESSAGEBIRD:
+                _send_messagebird_message.submit(to_phone, message)
+            if channel == ChannelType.AFRICAS_TALKING:
+                _send_at_message.submit(to_phone, message)
+        else:
+            print(f'"IS NOT PRODUCTION", not sending SMS:\n{message}')
--- app/server/utils/search.py
+++ app/server/utils/search.py
@@ -64,34 +64,3 @@ def generate_search_query(search_string, filters, order, sort_by_arg, include_us
         SearchableColumn('location', User.location, rank=1),
         SearchableColumn('primary_blockchain_address', User.primary_blockchain_address, rank=2),
     ]
-    sum_search = reduce(lambda x,y: x+y, [sc.get_similarity_query(search_string) for sc in user_search_columns])
-    sort_by = sum_search if sort_by_arg == 'rank' else sort_types_to_database_types[search_type][sort_by_arg]
-    if search_type == TRANSFER_ACCOUNT:
-        sort_by = sort_types_to_database_types[search_type]['date_account_created'] if sort_by_arg == 'rank' and not search_string else sum_search
-        entities = [TransferAccount, sum_search, User] if include_user else [TransferAccount]
-        final_query = db.session.query(TransferAccount, User, sum_search)\
-            .outerjoin(TransferAccount, User.default_transfer_account_id == TransferAccount.id)\
-            .filter(TransferAccount.is_ghost != True) \
-            .with_entities(*entities)\
-            .order_by(order(sort_by))
-        if include_user:
-            final_query = final_query.options(lazyload(User.custom_attributes))
-
-    else:
-        sort_by = sort_types_to_database_types[search_type]['id'] if sort_by_arg == 'rank' and not search_string else sort_by
-        if search_string:
-            final_query = db.session.query(CreditTransfer, sum_search)\
-                .join(sender, sender.id == User.id)\
-                .join(recipient, recipient.id == User.id)\
-                .join(CreditTransfer, or_((recipient.id == CreditTransfer.recipient_user_id), (sender.id == CreditTransfer.sender_user_id)))\
-                .with_entities(CreditTransfer)\
-                .order_by(order(sort_by))
-        else:
-            final_query = db.session.query(CreditTransfer)\
-                .order_by(order(sort_by))
-
-    final_query = final_query.filter(sum_search!=0) if search_string else final_query
-    if search_type == CREDIT_TRANSFER:
-        return apply_filters(final_query, filters, CreditTransfer)
-    if search_type == TRANSFER_ACCOUNT:
-        return apply_filters(final_query, filters, User)
