45caff2560cfa5578aef01caecea99dd39e92c0a
Cral_Cactus <marian.pb@abv.bg>
2024-07-28 21:51:10 +0300

    Finished filter module client components

--- app/client/components/filterModule/filter.jsx
+++ app/client/components/filterModule/filter.jsx
@@ -145,54 +145,308 @@ class Filter extends React.Component {
       el => possibleFilters[el]["sender_or_recipient"] === "sender"
     );
 
-    return (
-      <Select
-        showSearch
-        defaultValue="Select Attribute"
-        onChange={this.handleAttributeSelectorChange}
-        style={{ width: "225px" }}
-        optionLabelProp="label"
-      >
-        {this.generateOptionSubList(otherKeys, possibleFilters)}
-        {this.generateOptionSubList(senderKeys, possibleFilters, "Sender")}
-        {this.generateOptionSubList(
-          recipientKeys,
-          possibleFilters,
-          "Recipient"
-        )}
-      </Select>
-    );
-  };
+    if (
+        filterType === USER_FILTER_TYPE.DISCRETE ||
+        filterType === USER_FILTER_TYPE.BOOLEAN_MAPPING
+      ) {
+        return <FilterText style={{ width: "45px" }}>one of</FilterText>;
+      }
+  
+      return (
+        <Select
+          disabled={this.props.disabled}
+          onChange={this.comparatorChange}
+          defaultValue={"="}
+          style={{ width: "150px" }}
+        >
+          <Option key={"="}>
+            {filterType === USER_FILTER_TYPE.DATE_RANGE ? "on" : "is equal to"}
+          </Option>
+          <Option key={"<"}>
+            {filterType === USER_FILTER_TYPE.DATE_RANGE
+              ? "before"
+              : "is less than"}
+          </Option>
+          <Option key={">"}>
+            {filterType === USER_FILTER_TYPE.DATE_RANGE
+              ? "after"
+              : "is greater than"}
+          </Option>
+        </Select>
+      );
+    };
+  
+    handleDiscreteSelect = values => {
+      this.setState({
+        discreteSelected: values
+      });
+    };
+  
+    handleDateSelect = (date, dateString) => {
+      this.setState({
+        date: date
+      });
+    };
+  
+    handleNumericSet = value => {
+      this.setState({
+        GtLtThreshold: value
+      });
+    };
+  
+    valuePicker = () => {
+      let { filterType, attribute } = this.state;
+  
+      if (attribute === "select") {
+        return <div />;
+      }
+  
+      if (
+        filterType === USER_FILTER_TYPE.DISCRETE ||
+        filterType === USER_FILTER_TYPE.BOOLEAN_MAPPING
+      ) {
+        return this.discreteValuePicker();
+      }
+  
+      if (filterType === USER_FILTER_TYPE.DATE_RANGE) {
+        return this.dateValuePicker();
+      }
+      return this.numericValuePicker();
+    };
+  
+    discreteValuePicker = () => {
+      let { possibleFilters } = this.props;
+  
+      let { attribute } = this.state;
+  
+      let valueArray =
+        typeof possibleFilters[attribute].values !== "undefined"
+          ? [...possibleFilters[attribute].values]
+          : [];
+  
+      return (
+        <Select
+          disabled={this.props.disabled}
+          mode="multiple"
+          style={{ minWidth: "238px" }}
+          placeholder="Please select"
+          onChange={this.handleDiscreteSelect}
+        >
+          {valueArray.length !== 0
+            ? valueArray.map(value => (
+                <Option key={value}>{replaceUnderscores(value)}</Option>
+              ))
+            : null}
+        </Select>
+      );
+    };
+  
+    dateValuePicker = () => {
+      return <DatePicker onChange={this.handleDateSelect} />;
+    };
+  
+    numericValuePicker = () => {
+      return (
+        <InputNumber
+          type="number"
+          style={{ width: "133px" }}
+          onChange={this.handleNumericSet}
+        />
+      );
+    };
+  
+    handleAddFilter = () => {
+      let id = this.state.filters.length + 1;
+      var newFilter;
+      if (
+        this.state.filterType === USER_FILTER_TYPE.DISCRETE ||
+        this.state.filterType === USER_FILTER_TYPE.BOOLEAN_MAPPING
+      ) {
+        newFilter = {
+          id: id,
+          attribute: this.state.attribute,
+          type: USER_FILTER_TYPE.DISCRETE,
+          allowedValues: this.state.discreteSelected
+        };
+      } else if (this.state.filterType === USER_FILTER_TYPE.DATE_RANGE) {
+        newFilter = {
+          id: id,
+          attribute: this.state.attribute,
+          type: this.state.comparator,
+          threshold: this.state.date.format("YYYY-MM-DD")
+        };
+      } else {
+        let value = parseFloat(this.state.GtLtThreshold);
+        newFilter = {
+          id: id,
+          attribute: this.state.attribute,
+          type: this.state.comparator,
+          threshold: value
+        };
+      }
+  
+      this.setState(
+        prevstate => ({
+          filters: [...prevstate.filters, newFilter],
+          selectorKeyBase: `${Math.random()}`,
+          ...this.baseRuleConstructionState
+        }),
+        () => {
+          this.props.onFiltersChanged(this.state.filters);
+        }
+      );
+    };
+  
+    addFilterBtn = () => {
+      let { filterType } = this.state;
+      let rowValues = Object.values({});
+      let numberSelected = rowValues.filter(Boolean).length;
+      let isSelected = numberSelected > 0 || filterType !== "of";
+      var addFilterBtn = <div />;
+      if (isSelected) {
+        addFilterBtn = (
+          <Button type="primary" onClick={this.handleAddFilter}>
+            Add
+          </Button>
+        );
+      }
+      return addFilterBtn;
+    };
+  
+    removeFilter = id => {
+      if (this.props.disabled) {
+        return;
+      }
+  
+      this.setState(
+        prevstate => ({
+          filters: prevstate.filters.filter(filter => filter.id !== parseInt(id))
+        }),
+        () => {
+          this.props.onFiltersChanged(this.state.filters);
+        }
+      );
+    };
+  
+    activeFilterBubbles = () => {
+      let { filters } = this.state;
+      if (filters) {
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              flexFlow: "row wrap"
+            }}
+          >
+            {filters.map((filter, index) => {
+              let attributeProperties = this.props.possibleFilters[
+                filter.attribute
+              ];
+  
+              let color =
+                attributeProperties.table === "credit_transfer"
+                  ? DefaultTheme.primary
+                  : DefaultTheme.alt;
+  
+              let filterVals = <div />;
+  
+              if (
+                filter.type === USER_FILTER_TYPE.DISCRETE ||
+                filter.type === USER_FILTER_TYPE.BOOLEAN_MAPPING
+              ) {
+                filterVals = (
+                  <BubbleText style={{ color: color }}>
+                    {filter.allowedValues.map((value, index) => {
+                      if (filter.allowedValues.length === index + 1) {
+                        return value;
+                      } else {
+                        return value + " or ";
+                      }
+                    })}
+                  </BubbleText>
+                );
+              }
 
-  attributeSelector = () => {
-    let { possibleFilters } = this.props;
-    const keys =
-      possibleFilters !== undefined && possibleFilters !== null
-        ? Object.keys(possibleFilters).filter(key => key !== "profile_picture")
-        : [];
-
-    return (
-      <div
-        key={this.state.selectorKeyBase + "AS"}
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          flexFlow: "row wrap"
-        }}
-      >
-        {this.optionListGenerator(keys, possibleFilters)}
-      </div>
-    );
-  };
-
-  comparatorChange = value => {
-    this.setState({ comparator: value });
-  };
-
-  filterTypePicker = () => {
-    let { filterType, attribute } = this.state;
-
-    if (attribute === "select") {
-      return <div />;
-    }
+              render() {
+                return (
+                  <div style={{ display: "flex", flexDirection: "column" }}>
+                    <div
+                      style={{
+                        display: this.props.disabled ? "none" : "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        flexFlow: "row wrap"
+                      }}
+                    >
+                      <PaddedInput>{this.attributeSelector()}</PaddedInput>
+            
+                      <PaddedInput>{this.filterTypePicker()}</PaddedInput>
+            
+                      <PaddedInput>{this.valuePicker()}</PaddedInput>
+            
+                      <PaddedInput>{this.addFilterBtn()}</PaddedInput>
+                    </div>
+                    {this.activeFilterBubbles()}
+                  </div>
+                );
+              }
+            }
+            
+            (Filter.defaultProps = defaultProps), (Filter.propTypes = propTypes);
+            
+            export default Filter;
+            
+            const PaddedInput = styled.div`
+              margin: 10px;
+            `;
+            
+            const FilterText = styled.p`
+              white-space: nowrap;
+              margin: 0;
+              font: 200 14px system-ui;
+              color: #777;
+              -webkit-user-select: none; /* Chrome all / Safari all */
+              -moz-user-select: none; /* Firefox all */
+              -ms-user-select: none; /* IE 10+ */
+              user-select: none;
+            `;
+            
+            const FilterBubble = styled.div`
+              display: flex;
+              align-items: center;
+              width: fit-content;
+              margin: 10px 10px 10px 10px;
+              height: 32px;
+              padding: 0 10px 0 0;
+              background-color: #fff;
+              border: 1px solid #d9d9d9;
+              border-radius: 2px;
+            `;
+            
+            const BubbleLeft = styled.div`
+              display: flex;
+              align-items: center;
+              height: 32px;
+              margin-left: -1px;
+              border-radius: 2px 0 0 2px;
+              background-color: #d9d9d9;
+            `;
+            
+            const BubbleText = styled.p`
+              white-space: nowrap;
+              margin: 0;
+              padding: 10px;
+              font: 200 14px system-ui;
+              color: #777;
+              -webkit-user-select: none; /* Chrome all / Safari all */
+              -moz-user-select: none; /* Firefox all */
+              -ms-user-select: none; /* IE 10+ */
+              user-select: none;
+            `;
+            
+            const SVG = styled.img`
+              width: 12px;
+              height: 12px;
+            `;
