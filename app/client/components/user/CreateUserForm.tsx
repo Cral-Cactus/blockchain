852df04b54d847003a637704960cb16e0f827dca
Cral_Cactus <marian.pb@abv.bg>
2024-07-30 22:31:56 +0300

    Finished create user form

--- app/client/components/user/CreateUserForm.tsx
+++ app/client/components/user/CreateUserForm.tsx
@@ -98,48 +98,189 @@ const CreateUserForm = (props: Props) => {
   } = props;
   let initialDisbursementAmount: JSX.Element;
 
-  if (defaultDisbursement > 0) {
-    initialDisbursementAmount = (
-      <Form.Item label="Initial Disbursement Amount" name="initialDisbursement">
-        <Input
-          addonAfter={
-            activeToken !== null && typeof activeToken !== "undefined"
-              ? activeToken.symbol
-              : null
-          }
-        />
-      </Form.Item>
-    );
+  <Input
+  addonAfter={
+    <QrReadingModal
+      updateData={(data: string) => setSerialNumber(data)}
+    />
   }
-
-  return (
+/>
+</Form.Item>
+<span>or</span>
+<Form.Item
+label="Phone Number"
+name="phone"
+valuePropName="value"
+hasFeedback
+dependencies={["publicSerialNumber"]}
+rules={[
+  ({ getFieldValue }) => ({
+    validator(_, value) {
+      if (!value && !getFieldValue("publicSerialNumber")) {
+        return Promise.reject(
+          "Must provide either phone number or ID number"
+        );
+      }
+      return FormValidation.antPhone(value);
+    }
+  })
+]}
+>
+<AdaptedPhoneInput isPhoneNumber />
+</Form.Item>
+<Form.Item
+label="Given Name(s)"
+name="firstName"
+rules={[{ required: true, message: "Please input first name" }]}
+>
+<Input />
+</Form.Item>
+<Form.Item label="Family/Surname" name="lastName">
+<Input />
+</Form.Item>
+<Form.Item label="Location" name="location">
+<Input />
+</Form.Item>
+<Form.Item label={"Gender"} name={"gender"}>
+<Select>
+  {Object.keys(GenderTypes).map((type, index) => {
+    return (
+      <Option value={type} key={index}>
+        {type}
+      </Option>
+    );
+  })}
+</Select>
+</Form.Item>
+<Form.Item
+noStyle
+shouldUpdate={(prevValues, currentValues) =>
+  prevValues.accountTypes !== currentValues.accountTypes
+}
+>
+{({ getFieldValue }) =>
+  getFieldValue("accountTypes") &&
+  getFieldValue("accountTypes").includes("beneficiary") ? (
+    <>{initialDisbursementAmount}</>
+  ) : null
+}
+</Form.Item>
+<Form.Item
+noStyle
+shouldUpdate={(prevValues, currentValues) =>
+  prevValues.accountTypes !== currentValues.accountTypes
+}
+>
+{({ getFieldValue }) =>
+  getFieldValue("accountTypes") &&
+  getFieldValue("accountTypes").includes("cashier") ? (
     <div>
-      <Form onFinish={onFinish} layout="vertical" form={form}>
-        <Form.Item label="Account Types" name="accountTypes">
-          <Select mode="multiple">
-            {Object.values(validRoles).map((value, index) => {
-              return (
-                <Option value={value} key={index}>
-                  {value}
-                </Option>
-              );
-            })}
-          </Select>
-        </Form.Item>
-        <Form.Item
-          label="ID Number"
-          name="publicSerialNumber"
-          dependencies={["phone"]}
-          rules={[
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value && !getFieldValue("phone")) {
-                  return Promise.reject(
-                    "Must provide either phone number or ID number"
+      <div>
+        To create a cashier account, enter the{" "}
+        <strong>vendor's</strong> phone and pin.
+      </div>
+      <Form.Item
+        label="Vendor Phone Number"
+        name="existingVendorPhone"
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item label="Vendor PIN" name="existingVendorPin">
+        <Input type="password" />
+      </Form.Item>
+    </div>
+  ) : null
+}
+</Form.Item>
+<Form.Item
+noStyle
+shouldUpdate={(prevValues, currentValues) =>
+  prevValues.accountTypes !== currentValues.accountTypes
+}
+>
+{({ getFieldValue }) =>
+  getFieldValue("accountTypes") &&
+  (getFieldValue("accountTypes").includes("vendor") ||
+    getFieldValue("accountTypes").includes("cashier") ||
+    getFieldValue("accountTypes").includes("supervendor")) ? (
+    <div>
+      {transferUsages.length > 0 ? (
+        <div>
+          <Form.Item name="businessUsage" label="Business Category">
+            <Select>
+              {optionizeUsages().map(
+                (
+                  usage: { value: string; name: React.ReactNode },
+                  index: string | number | undefined
+                ) => {
+                  return (
+                    <Option value={usage.value} key={index}>
+                      {usage.name}
+                    </Option>
                   );
                 }
-                return Promise.resolve();
-              }
-            })
-          ]}
-        ></Form.Item>
+              )}
+            </Select>
+          </Form.Item>
+          <Form.Item
+            noStyle
+            shouldUpdate={(prevValues, currentValues) =>
+              prevValues.businessUsage !== currentValues.businessUsage
+            }
+          >
+            {({ getFieldValue }) =>
+              getFieldValue("businessUsage") &&
+              getFieldValue("businessUsage").toLowerCase() ===
+                "other" ? (
+                <Form.Item
+                  name="usageOtherSpecific"
+                  label="Please specify the category"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please specify the category!"
+                    }
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+              ) : null
+            }
+          </Form.Item>
+        </div>
+      ) : null}
+      <Form.Item label="Store Name" name="transferAccountName">
+        <Input />
+      </Form.Item>
+    </div>
+  ) : null
+}
+</Form.Item>
+<Form.Item>
+<Button
+  htmlType="submit"
+  type="primary"
+  loading={props.users.createStatus.isRequesting}
+>
+  Submit
+</Button>
+</Form.Item>
+</Form>
+</div>
+);
+};
+
+const mapStateToProps = (state: ReduxState): StateProps => {
+return {
+activeToken: getActiveToken(state),
+defaultDisbursement:
+// @ts-ignore
+state.organisations.byId[state.login.organisationId]
+.default_disbursement / 100,
+validRoles:
+// @ts-ignore
+state.organisations.byId[state.login.organisationId].valid_roles || []
+};
+};
+
+export default connect(mapStateToProps)(CreateUserForm);
